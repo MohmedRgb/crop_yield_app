531bd380e90ac7aa072a4ce1cbfa413ef11f4661
Mohamed Ragab <mohamedragab9r0@gmail.com>
2025-04-27 02:26:04 +0300

    Add files via upload

---
+++ predict_image.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import numpy as np
+import json
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+
+model = load_model("best_model.keras")
+
+# تحميل أسماء الكلاسات
+with open("class_names.json") as f:
+    class_indices = json.load(f)
+class_names = [k for k, v in sorted(class_indices.items(), key=lambda item: item[1])]
+
+def prepare_image(img_path, target_size=(224, 224)):
+    img = image.load_img(img_path, target_size=target_size)
+    img_array = image.img_to_array(img) / 255.0
+    return np.expand_dims(img_array, axis=0)
+
+def predict_disease(img_path):
+    processed_img = prepare_image(img_path)
+    prediction = model.predict(processed_img)
+    predicted_class = class_names[np.argmax(prediction)]
+    return predicted_class
