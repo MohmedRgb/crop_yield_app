ec01d411d58be9d8fbddefd3fd6bba655b7a419c
Mohamed Ragab <mohamedragab9r0@gmail.com>
2025-04-27 06:40:26 +0300

    Update predict_image.py

--- predict_image.py
+++ predict_image.py
@@ -3,23 +3,13 @@ import numpy as np
 import json
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
-import os
 
-model = None
-class_names = None
+model = load_model("best_model.keras")
 
-def load_model_and_classes():
-    global model, class_names
-    if model is None:
-        print("Loading model and class names...")
-        if not os.path.exists("best_model.keras"):
-            raise FileNotFoundError("ERROR: Model file not found!")
-        model = load_model("best_model.keras")
-        
-        with open("class_names.json") as f:
-            class_indices = json.load(f)
-        class_names = [k for k, v in sorted(class_indices.items(), key=lambda item: item[1])]
-        print("Model and classes loaded.")
+# تحميل أسماء الكلاسات
+with open("class_names.json") as f:
+    class_indices = json.load(f)
+class_names = [k for k, v in sorted(class_indices.items(), key=lambda item: item[1])]
 
 def prepare_image(img_path, target_size=(224, 224)):
     img = image.load_img(img_path, target_size=target_size)
@@ -27,7 +17,6 @@ def prepare_image(img_path, target_size=(224, 224)):
     return np.expand_dims(img_array, axis=0)
 
 def predict_disease(img_path):
-    load_model_and_classes()
     processed_img = prepare_image(img_path)
     prediction = model.predict(processed_img)
     predicted_class = class_names[np.argmax(prediction)]
